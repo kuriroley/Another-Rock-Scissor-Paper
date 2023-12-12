3d4f2996e256f45b072fa09288ddfad7562f82f2
kuriroley <christian.rodriguez@tutanota.com>
2023-12-12 17:03:42 +0900

    changes on ui and game logic

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const cardButtons = document.querySelectorAll('buttons');
+const cardButtons = document.querySelectorAll('button');
+const selectLog = document.querySelector('span');
+const pScore = document.querySelector('.pScore');
+const cScore = document.querySelector('.cScore');
 
+cardButtons.forEach((button)=>{
+    let playerAnswer;
+    
+    button.addEventListener('click', e=>{
+        playerAnswer = e.target.value;
+        gameLogic(playerAnswer,getComputerChoice());
 
-
+    })
+})
 
 function getComputerChoice(){
     let choices = ['rock','scissor','paper'];
@@ -9,39 +19,29 @@ function getComputerChoice(){
     let choice = choices[Math.floor(choices.length * Math.random())];
     return choice
 }
-
-
-function getPlayerChoice(choice){
-    let playerAnswer = prompt();
-     return playerAnswer.toLowerCase();
-
-}
-
+let gameRounds = 0;
+let playerScore = 0;
+let computerScore = 0;
 function gameLogic(player,computer){
-    let gameCount = 0;
-    let playerScore = 0;
-    let computerScore = 0;
-        if(player === 'rock' && computer === 'scissor'){
-            playerScore++;
-            gameCount++;
-            return console.log(`player's rock beat computer's scissor`);
-            
-        } else if( player === 'scissor' && computer === 'paper'){
-            playerScore++;
-            gameCount++;
-            return console.log(`player's scissor beat computer's paper`)
-            
-        } else if( player === 'paper' && computer === 'rock'){
-            playerScore++;
-            gameCount++;
-            return console.log(`player's paper beat computer's rock`)
-            
-        } else{
-            computerScore++;
-            gameCount++;
-            return console.log(`Computer's ${computer} beat player's ${player}!`);
-            
-        }
+    if(player === 'rock' && computer === 'scissor'){
+        playerScore++;
+        pScore.textContent = `Player score: ${playerScore}`;
+        return selectLog.textContent = `Player's rock beat computer's scissor!`        
+    } else if( player === 'scissor' && computer === 'paper'){
+        playerScore++;
+        pScore.textContent = `Player score: ${playerScore}`;
+        return selectLog.textContent = `Player's scissor beat computer's paper`
+    } else if( player === 'paper' && computer === 'rock'){
+        playerScore++;
+        pScore.textContent = `Player score: ${playerScore}`;
+        return selectLog.textContent = `Player's paper beat computer's rock`
+    }  else if (player === computer){
+        return selectLog.textContent = `Player's ${player} and Computer's ${computer} are the same!`;        
+    } else{
+        cScore.textContent = `Computer score: ${computerScore}`;
+        return selectLog.textContent = `Computer's ${computer} beat player's ${player}! Try again!`;        
+    }
+        
 }
 
 // for (let i = 1; i<=5; i++){
